5769da35a4de724a4507a2511901cc4b74d34bf0
Avenwes <anavas@gmail.com>
2022-10-23 16:15:24 +0500

    birdsNotes 0.1.5

---
+++ app/src/main/java/com/practicum/birdsnotes/DataBase/RoomDB.java
@@ -0,0 +1,35 @@
+package com.practicum.birdsnotes.DataBase;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomMasterTable;
+
+import com.practicum.birdsnotes.Models.Notes;
+
+@Database(entities = Notes.class, version = 1, exportSchema = false)
+public abstract class RoomDB extends RoomDatabase {
+
+    private static RoomDB database;
+    private static String DATABASE_NAME = "NoteApp";
+
+    public synchronized static RoomDB getInstance(Context context){
+        if (database == null){
+            database = Room.databaseBuilder(context.getApplicationContext(), RoomDB.class, DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return database;
+
+    }
+
+    public abstract mainDAO mainDAO();
+
+
+
+
+
+}
